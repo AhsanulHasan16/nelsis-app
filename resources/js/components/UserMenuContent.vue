c77c351db2c97f562583446a6e3b268108454d07
Md. Ahsanul Hasan <82327664+AhsanulHasan16@users.noreply.github.com>
2025-06-29 06:23:35 +0600

    Update UserMenuContent.vue

--- resources/js/components/UserMenuContent.vue
+++ resources/js/components/UserMenuContent.vue
@@ -23,15 +23,15 @@ defineProps<Props>();
         </div>
     </DropdownMenuLabel>
     <DropdownMenuSeparator />
-    <DropdownMenuGroup>
+    <!-- <DropdownMenuGroup>
         <DropdownMenuItem :as-child="true">
             <Link class="block w-full" :href="route('profile.edit')" prefetch as="button">
                 <Settings class="mr-2 h-4 w-4" />
                 Settings
             </Link>
         </DropdownMenuItem>
-    </DropdownMenuGroup>
-    <DropdownMenuSeparator />
+    </DropdownMenuGroup> -->
+    <!-- <DropdownMenuSeparator /> -->
     <DropdownMenuItem :as-child="true">
         <Link class="block w-full" method="post" :href="route('logout')" @click="handleLogout" as="button">
             <LogOut class="mr-2 h-4 w-4" />
